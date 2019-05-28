d427bd1e8bf1843745cbda994525a6cd8f33d56c
newbreach <newbreach@live.com>
2019-05-28 22:58:15 +0800

    加入pin

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,14 +5,22 @@ var service = require('../../services/device');
 
 /* 启动水泵 */
 router.post('/open_pump', function (req, res, next) {
-  service.openPump("0000000055ed");
-  res.send(JSON.stringify({}));
+  if (req.body.pin === '0923') {
+    service.openPump("0000000055ed");
+    res.send(JSON.stringify({ code: 1000 }));
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
 });
 
 /* 关闭水泵 */
 router.post('/close_pump', function (req, res, next) {
-  service.closePump("0000000055ed");
-  res.send(JSON.stringify({}));
+  if (req.body.pin === '0923') {
+    service.closePump("0000000055ed");
+    res.send(JSON.stringify({ code: 1000 }));
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
 });
 
 module.exports = router;
