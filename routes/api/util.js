00addd2d65d862dd70e019588d1b2cdc5ae70a7c
newbreach <newbreach@live.com>
2019-06-14 00:10:17 +0800

    change

--- routes/api/util.js
+++ routes/api/util.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/get_ip', function (req, res, next) {
-  let ip = req.socket.localAddress;
+  let ip = req.socket.remoteAddress;
   res.send(JSON.stringify({ code: 1000, data: ip }));
 });
 
