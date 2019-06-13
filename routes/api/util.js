a11a587fa4863f3bc3a0b9c4edebd8cc3a1aef5a
newbreach <newbreach@live.com>
2019-06-14 00:21:05 +0800

    change get

--- routes/api/util.js
+++ routes/api/util.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var util = require('../../utils/index');
 
-router.post('/get_ip', function (req, res, next) {
+router.get('/get_ip', function (req, res, next) {
   res.send(JSON.stringify({ code: 1000, data: util.getClientIp(req) }));
 });
 
