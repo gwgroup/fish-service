3bdfe3f951e68874376924d728cee9bbe59cb1be
newbreach <newbreach@live.com>
2019-06-14 00:07:16 +0800

    api/util getip

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api/index');
-
+var apiUtilRouter = require('./routes/api/util');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use('/api/util', apiUtilRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
---
+++ routes/api/util.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/get_ip', function (req, res, next) {
+  let ip = req.socket.localAddress;
+  res.send(JSON.stringify({ code: 1000, data: ip }));
+});
+
+module.exports = router;
