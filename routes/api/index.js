e244d9d0421b7c94fa4439c67af44829ddc0da60
newbreach <newbreach@live.com>
2019-07-14 19:35:54 +0800

    加入增氧机和灯

--- routes/api/index.js
+++ routes/api/index.js
@@ -41,6 +41,159 @@ router.post('/close_pump', function (req, res, next) {
   }
 });
 
+
+/* 启动增氧机1 */
+router.post('/open_aerator1', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.aerator1 === 1) {
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.openAerator1(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+/* 关闭增氧机1 */
+router.post('/close_aerator1', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.aerator1 === 0) {
+     
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.closeAerator1(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+
+/* 启动增氧机2 */
+router.post('/open_aerator2', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.aerator2 === 1) {
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.openAerator2(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+/* 关闭增氧机2 */
+router.post('/close_aerator2', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.aerator2 === 0) {
+     
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.closeAerator2(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+
+/* 启动LED灯1 */
+router.post('/open_lamp1', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.lamp1 === 1) {
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.openLamp1(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+/* 关闭LED灯1 */
+router.post('/close_lamp1', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.lamp1 === 0) {
+     
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.closeLamp1(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+
+/* 启动LED灯2 */
+router.post('/open_lamp2', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.lamp2 === 1) {
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.openLamp2(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+/* 关闭LED灯2 */
+router.post('/close_lamp2', function (req, res, next) {
+  if (req.body.pin === configPin) {
+    let obj = service.deviceInfo(CLIENT_ID);
+    if (!obj || obj.online === 0) {
+      //设备不在线
+      res.send(JSON.stringify({ code: 1002 }));
+    } else if (obj.status.lamp2 === 0) {
+     
+      res.send(JSON.stringify({ code: 1003 }));
+    } else {
+      service.closeLamp2(CLIENT_ID);
+      res.send(JSON.stringify({ code: 1000 }));
+    }
+  } else {
+    res.send(JSON.stringify({ code: 1001 }));
+  }
+});
+
+
 /**
  * 设备信息
  */
--- services/device.js
+++ services/device.js
@@ -42,6 +42,81 @@ function closePump(clientId) {
   mqtt.sendWithClient(clientId, { type: 3002 });
 }
 
+
+/**
+ * 开增氧机1
+ * @param {*} clientId 
+ */
+function openAerator1(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3031 });
+}
+
+
+/**
+ * 关增氧机1
+ * @param {*} clientId 
+ */
+function closeAerator1(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3041 });
+}
+
+/**
+ * 开增氧机2
+ * @param {*} clientId 
+ */
+function openAerator2(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3032 });
+}
+
+/**
+ * 关增氧机2
+ * @param {*} clientId 
+ */
+function closeAerator2(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3042 });
+}
+
+/**
+ * 开LED1
+ * @param {*} clientId 
+ */
+function openLamp1(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3011 });
+}
+
+/**
+ * 关LED1
+ * @param {*} clientId 
+ */
+function closeLamp1(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3021 });
+}
+
+
+/**
+ * 开LED2
+ * @param {*} clientId 
+ */
+function openLamp2(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3012 });
+}
+
+/**
+ * 关LED2
+ * @param {*} clientId 
+ */
+function closeLamp2(clientId) {
+  mqtt.sendWithClient(clientId, { type: 3022 });
+}
+
+/**
+ * 更新重启
+ * @param {*} clientId 
+ */
+function updateReset(clientId) {
+  mqtt.sendWithClient(clientId, { "type": 3004, "index": 10004, "cmd": "/home/work/script/fish-client.auto.update.sh" });
+}
+
 /**
  * 获取设备信息
  * @param {String} clientId 
