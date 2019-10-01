3fc2e691809dcd3311b84c5e6a3d21b7bb2b1c07
newbreach <newbreach@live.com>
2019-10-01 16:14:51 +0800

    fill report

--- services/report.js
+++ services/report.js
@@ -1,9 +1,36 @@
+var util = require('../utils/index'),
+  MysqlHelper = util.MysqlHelper;
+/*
+
+
+{ start_time: 1569917055526,
+data:    bin/www:6182 -   end_time: 1569917056527,
+data:    bin/www:6182 -   plan_duration: 1000,
+data:    bin/www:6182 -   io_code: 'feeder1',
+data:    bin/www:6182 -   io_name: '投喂机1',
+data:    bin/www:6182 -   io_type: 'feeder',
+data:    bin/www:6182 -   weight_per_second: 23.1 }
+
+{ start_time: 1569917008813,
+data:    bin/www:6182 -   end_time: 1569917018814,
+data:    bin/www:6182 -   plan_duration: 10000,
+data:    bin/www:6182 -   io_code: 'lamp1',
+data:    bin/www:6182 -   io_name: '灯',
+data:    bin/www:6182 -   io_type: 'lamp' }
+
+
+*/
+
 /**
  * 采集报表
  * @param {String} clientId 
  * @param {Object} report 
  */
 function fill(clientId, report) {
-  console.log(clientId, report);
+  MysqlHelper.query('INSERT INTO `fish`.`f_report` SET ?;', Object.assign({ device_mac: clientId }, report), (err) => {
+    if (err) {
+      console.error(err, clientId, report);
+    }
+  });
 }
 module.exports = { fill };
