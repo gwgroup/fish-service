d678eac23bd1b74769f8fd5e8e8a38a77b9f1944
newbreach <newbreach@live.com>
2019-10-10 02:36:37 +0800

    file save

--- routes/api/util.js
+++ routes/api/util.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var util = require('../../utils/index');
+var fs = require('fs');
 var multer = require('multer');
 var path = require('path');
 var fishScreenshot = multer({ dest: path.join(__dirname, '../fish-screenshot/') });
 var deviceService = require('../../services/device');
 var easyDarwinService = require('../../services/easy-darwin');
+const SCREENSHOT_URL = require('../../config/index').openUrls.screenshotUrl;
 //上报设备信息
 router.get('/get_info', function (req, res, next) {
   let client_id = req.param("client_id");
@@ -26,9 +28,7 @@ router.get('/ed_stream_can_be_release', function (req, res, next) {
 });
 //文件上传
 router.post('/file_upload', fishScreenshot.single('screenshot'), function (req, res, next) {
-  // req.file 是 `avatar` 文件的信息
-  // req.body 将具有文本域数据，如果存在的话
-  console.log(req.file);
-  res.send(JSON.stringify({ code: 1000, data: 'xxx' }));
+  fs.renameSync(req.file.path, path.join(req.file.destination, req.file.originalname));
+  res.send(JSON.stringify({ code: 1000, data: `${SCREENSHOT_URL}/${req.file.originalname}` }));
 });
 module.exports = router;
